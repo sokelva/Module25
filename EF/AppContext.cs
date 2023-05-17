a1729558148f1052c4cf7734e778b762f092ebfe
sokelva <sokelva@mail.ru>
2023-05-17 16:55:11 +0300

    Добавлены новые классы.

---
+++ EF/AppContext.cs
@@ -0,0 +1,21 @@
+﻿using EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApp
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=.;Database=EF;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EF/Program.cs
@@ -0,0 +1,22 @@
+﻿using FirstApp;
+using EF;
+using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new FirstApp.AppContext())
+            {
+                var user1 = new User { Name = "Arthur", Role = "Admin" };
+                var user2 = new User { Name = "klim", Role = "User" };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+            }
+        }
+    }
+}
